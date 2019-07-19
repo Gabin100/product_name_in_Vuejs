18d03287fd17d76ba74df6142b57ce1b4787c6be
Jean Gabin <ij.gabin123@gmail.com>
2019-07-19 16:08:07 +0200

    quantity

--- public/javascipt/main.js
+++ public/javascipt/main.js
@@ -1,19 +1,21 @@
 var app = new Vue({
     el:'#app',
     data:{
+        brand: "Vue Master",
         product: 'Socks',
-        image:'./public/images/vmSocks-blue.png',
-        inventory:4,
+        selectedValiant: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants:[
             {
                 variantId:2234,
                 variantColor:"Green",
-                variantImage:"./public/images/vmSocks-green.png"
+                variantImage:"./public/images/vmSocks-green.png",
+                variantQuantity:10
             },{
                 variantId:2235,
                 variantColor:"Blue",
-                variantImage:"./public/images/vmSocks-blue.png"
+                variantImage:"./public/images/vmSocks-blue.png",
+                variantQuantity:0
             }
         ],
         cart:0
@@ -21,8 +23,20 @@ var app = new Vue({
     methods:{
         addToCart(){
             this.cart +=1
-        },updateProductImg(variantImage){
-                this.image = variantImage
+        },updateProductImg(index){
+                this.selectedValiant = index
+                console.log(index)
             }
+    },
+    computed:{
+        title() {
+            return this.brand + ' ' + this.product
+        },
+    image(){
+        return this.variants[this.selectedValiant].variantImage 
+    },
+    inStock(){
+        return this.variants[this.selectedValiant].variantQuantity
+    }
     }
 })
