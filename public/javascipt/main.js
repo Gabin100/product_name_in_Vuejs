1ed85997712d70e56ab8db4d5f11ce4a5c0aeea7
Jean Gabin <ij.gabin123@gmail.com>
2019-07-19 14:06:57 +0200

    add boostrap css and jquery and v-if, v-show

--- public/javascipt/main.js
+++ public/javascipt/main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el:'#app',
     data:{
         product: 'Socks',
-        image:'./public/images/vmSocks-blue.png'
+        image:'./public/images/vmSocks-blue.png',
+        inventory:0
     }
 })
