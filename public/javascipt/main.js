e74b27d9db20a8045d3dac94d95e47f822a48502
Jean Gabin <ij.gabin123@gmail.com>
2019-07-20 11:15:31 +0200

    forms required

--- public/javascipt/main.js
+++ public/javascipt/main.js
@@ -32,6 +32,22 @@ Vue.component('product',{
                 :disabled="!inStock"
                 :class="{disabled:!inStock}">Add to cart</button>
             </div>
+            <div class="card col-12 mt-3 mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">Reviews</h5>
+                    <h6 class="card-subtitle mb-2 text-muted" v-if="!reviews.length">There are No reviews yet.</h6>
+                    <p class="card-text">
+                        <ul>
+                            <li v-for="review in reviews">
+                            <p><strong>{{review.name}}</strong></p>
+                            <p>Rating: {{review.rating}}</p>
+                            <p>{{review.review}}</p>
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+            <product-review @review-submitted="addReview"></product-review'>
         </div>
     `,
     data(){
@@ -52,16 +68,21 @@ Vue.component('product',{
                     variantImage:"./public/images/vmSocks-blue.png",
                     variantQuantity:0
                 }
-            ]
+            ],
+            reviews:[]
         }
     },
     methods:{
         addToCart(){
             this.$emit('add-to-cart', this.variants[this.selectedValiant].variantId)
-        },updateProductImg(index){
+        },
+        updateProductImg(index){
                 this.selectedValiant = index
                 console.log(index)
-            }
+        },
+        addReview(productReview){
+            this.reviews.push(productReview)
+        }
     },
     computed:{
         title() {
@@ -82,7 +103,68 @@ Vue.component('product',{
     }
     }
 })
-
+Vue.component('product-review', {
+    template:`
+    <form class="review-form" @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors" class="text-danger">{{error}}</li>
+            </ul>
+        </p>
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input class="form-control" id="name" placeholder="Your name" v-model="name">
+        </div>
+        <div class="form-group">
+            <label for="review">Review:</label>
+            <textarea id="review" class="form-control" v-model="review"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="rating">select Rating:</label>
+            <select class="form-control" id="rating" v-model.number="rating">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="form-control btn btn-primary" value="Submit">
+        </div>        
+  </form>
+    
+    `,
+    data(){
+        return{
+            name: null,
+            review: null,
+            rating: null,
+            errors:[]
+        }
+    },
+    methods:{
+        onSubmit(){
+            if(this.name && this.review && this.rating){
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            }
+            else{
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.review) this.errors.push("Review required.")
+            }
+        }
+    }
+})
 var app = new Vue({
     el:'#app',
     data:{
